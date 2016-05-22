e479034aeb81f753293bcfdf7b1e2a3ddae03f93
proudzhu <proudzhu.fdu@gmail.com>
2016-05-22 10:17:30 +0800

    add Problem 10

--- src/Haskell99.hs
+++ src/Haskell99.hs
@@ -129,3 +129,15 @@ compress = map head . group
 -- ["aaaa","b","cc","aa","d","eeee"]
 pack :: (Eq a) => [a] -> [[a]]
 pack = group
+
+-- | Problem 10
+-- Run-length encoding of a list.
+-- Use the result of problem P09 to implement the so-called run-length
+-- encoding data compression method.
+-- Consecutive duplicates of elements are encoded as lists (N E) where N is
+-- the number of duplicates of the element E.
+--
+-- >>> encode "aaaabccaadeeee"
+-- [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode lst = map (\x -> (length x, head x)) $ pack lst
